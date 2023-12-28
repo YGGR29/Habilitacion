ad90d0ff814bc44210c7b08edec75b3f15c0f0b4
YGGR29 <134571761+YGGR29@users.noreply.github.com>
2023-12-27 19:11:13 -0500

    Delete novato directory

--- novato/src/main/java/com/example/demo/repositories/ClaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Clase;
-
-public interface ClaseRepository extends JpaRepository<Clase, Long> {
-}
